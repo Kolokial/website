d034aa974ad3d11be7914999a84d083ef9c39bd8
Kolokial <gary_luck@me.com>
2019-01-26 23:02:45 +0000

    tidied up.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,19 @@ import { NgModule } from '@angular/core';
 import { IconListComponent } from './components/skillList/skillList.component';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { PhotosComponent } from './views/photos/photos.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
+import { RouterModule, } from '@angular/router';
 import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
 
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'photos', component: PhotosComponent}
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     IconListComponent,
-    HomeComponent,
-    PhotosComponent,
     ImageGalleryComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    RouterModule.forRoot(
-      appRoutes,
-      // { enableTracing: true } // <-- debugging purposes only
-    )
   ],
   providers: [],
   bootstrap: [AppComponent]
