f2710d799b12de8a8f2148f9468c99b87fc108a0
koloqial <koloqial@icloud.com>
2025-02-10 22:37:41 +0000

    adding font awesome

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 export interface Tile {
   color: string;
@@ -12,13 +17,19 @@ export interface Tile {
 }
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, MatCardModule, MatButtonModule, MatGridListModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    FontAwesomeModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Gary Luck';
+  faCoffee = faCoffee;
 
   tiles: Tile[] = [
     { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
